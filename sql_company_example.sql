e5eb87e3ec24be3addfdb7c345bdfc542ce9bd16
Karim Wölbert <karimwolbert@MacBook-Pro.fritz.box>
2020-10-11 21:44:06 +0200

    added file

---
+++ sql_company_example.sql
@@ -0,0 +1,71 @@
+CREATE TABLE employee (
+    emp_id INT PRIMARY KEY,
+    first_name VARCHAR(40),
+    last_name VARCHAR(40),
+    birth_date DATE,
+    sex VARCHAR(1),
+    salary INT,
+    super_id INT,
+    branch_id INT
+);
+
+CREATE TABLE branch (
+    branch_id INT PRIMARY KEY,
+    branch_name VARCHAR(40),
+    mgr_id INT,
+    mgr_start_date DATE,
+    FOREIGN KEY(mgr_id) REFERENCES employee(emp_id) ON DELETE SET NULL
+);
+
+ALTER TABLE employee
+ADD FOREIGN KEY(branch_id)
+REFERENCES branch(branch_id)
+ON DELETE SET NULL;
+
+ALTER TABLE employee
+ADD FOREIGN KEY(super_id)
+REFERENCES employee(emp_id)
+ON DELETE SET NULL;
+
+CREATE TABLE client (
+    client_id INT PRIMARY KEY,
+    client_name VARCHAR(40),
+    branch_id INT,
+    FOREIGN KEY(branch_id)
+    REFERENCES branch(branch_id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE works_with (
+    emp_id INT,
+    client_id INT,
+    total_sales INT,
+    PRIMARY KEY(emp_id, client_id),
+    FOREIGN KEY(emp_id)
+    REFERENCES employee(emp_id)
+    ON DELETE CASCADE,
+    FOREIGN KEY(client_id)
+    REFERENCES client(client_id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE branch_supplier (
+    branch_id INT,
+    supplier_name VARCHAR(40),
+    supply_type VARCHAR(40),
+    PRIMARY KEY(branch_id, supplier_name),
+    FOREIGN KEY(branch_id)
+    REFERENCES branch(branch_id)
+    ON DELETE CASCADE
+);
+
+# have to respect the order of the commands because branch id in branch didn't exists at the start so we have to give it a NULL for start
+INSERT INTO employee VALUES(100, 'Karim', 'Wölbert', '1992-06-26', 'M', 250000, NULL, NULL);
+
+INSERT INTO branch VALUES (1, 'Corporate', 100, '2006-02-09');
+
+UPDATE employee
+SET branch_id = 1
+WHERE emp_id = 100;
+
+INSERT INTO employee VALUES(101, 'Jan', 'Levinson', '1961-05-11', 'F', 110000, 100, 1)
